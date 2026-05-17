36f3ea3f29e56f207a339c6e8692fa0197553008
Daniel Ceriana <60513314+Daniel-Ceriana@users.noreply.github.com>
2026-05-17 11:16:02 -0300

    Funciones strings hasta trim 15-05

--- AlgoritmosAFondo_EMPTY_v2.7.36/biblioteca/funciones/strings.hpp
+++ AlgoritmosAFondo_EMPTY_v2.7.36/biblioteca/funciones/strings.hpp
@@ -7,160 +7,486 @@
 
 #include <iostream>
 using std::string;
+// using std::to_string;//=======================
 
 int length(string s)
 {
-    return 0;
+    int i = 0;
+    while(s[i] != '\0')
+    {
+        i++;
+    }
+
+    return i;
 }
 
 int charCount(string s, char c)
 {
-    return 0;
+    int i = 0;
+    int contador = 0;
+    while(i != length(s))
+    {
+        if(s[i] == c)
+        {
+            contador++;
+        }
+        i++;
+    }
+
+    return contador;
 }
 
 string substring(string s, int d, int h)
 {
-    return "";
+    string retorno;
+
+    while(d < h)
+    {
+        retorno += s[d];
+        d++;
+    }
+
+    return retorno;
 }
 
 string substring(string s, int d)  // ok
 {
-    return "";
+    return substring(s, d, length(s));
 }
 
 int indexOf(string s, char c)  // ok
 {
-    return 0;
+    int i = 0;
+    int retorno = -1;
+    while(s[i] != c && s[i] != '\0')
+    {
+        i++;
+    }
+    if(s[i] == c)
+    {
+        retorno = i;
+    }
+    return retorno;
 }
 
-int indexOf(string s, char c, int offSet)  // ok
+int indexOf(string s, char c,
+            int offSet)  // ok // MEJORAR (llamando a la anterior)
 {
+    int i = offSet;
+    int retorno = -1;
+    while(s[i] != c && s[i] != '\0')
+    {
+        i++;
+    }
+    if(s[i] == c)
+    {
+        retorno = i;
+    }
+    return retorno;
+
     return 0;
 }
 
 int indexOf(string s, string toSearch)  // ok
 {
-    return 0;
+    int indice = -1;
+    int i = 0;
+    int j = 0;
+    bool continuar = true;
+
+    while(continuar)
+    {
+        if(toSearch[j + 1] == '\0' || s[i] == '\0')
+        {
+            continuar = false;
+        }
+
+        if(s[i] == toSearch[j])
+        {
+            j++;
+            if(indice == -1)
+            {
+                indice = i;
+            }
+        }
+        else
+        {
+            j = 0;
+            indice = -1;
+        }
+
+        i++;
+    }
+
+    return indice;
 }
 
-int indexOf(string s, string toSearch, int offset)
+int indexOf(string s, string toSearch,
+            int offset)  // MEJORAR (llamando a la anterior)
 {
-    return 0;
+    int indice = -1;
+    int i = offset;
+    int j = 0;
+    bool continuar = true;
+
+    while(continuar)
+    {
+        if(toSearch[j + 1] == '\0' || s[i] == '\0')
+        {
+            continuar = false;
+        }
+
+        if(s[i] == toSearch[j])
+        {
+            j++;
+            if(indice == -1)
+            {
+                indice = i;
+            }
+        }
+        else
+        {
+            j = 0;
+            indice = -1;
+        }
+
+        i++;
+    }
+
+    return indice;
 }
 
 int lastIndexOf(string s, char c)
 {
-    return 0;
+    // Da vuelta el string, se lo pasa al uno de los buscadores de indice
+    // anteriores hace la diferencia de indices (para ver cual es el que
+    // corresponde al string original) devuelve ese resultado
+    int i = length(s) - 1;
+    string aux;
+    int retorno = -1;
+    int indiceEncontrado;
+    while(i >= 0)
+    {
+        aux += s[i];
+        i--;
+    }
+    indiceEncontrado = indexOf(aux, c);
+    if(indiceEncontrado >= 0)
+    {
+        retorno = length(s) - 1 - indexOf(aux, c);
+        // Indices totales //-// Indices desde el final
+    }
+
+    return retorno;
 }
 
 int indexOfN(string s, char c, int n)
 {
-    return 0;
+    if(n == 0)
+    {
+        return -1;
+    }
+
+    int cantOcurrencias = charCount(s, c);
+    if(n > cantOcurrencias)
+    {
+        return length(s);
+    }
+    // En esos dos casos, prefiero retornar rapido, asi evito tener un codigo
+    // muy anidado por los ifs.
+
+    int i = 0;
+    int j = 0;
+    int retorno = 0;
+
+    while(j < n && s[i] != '\0')
+    {
+        if(s[i] == c)
+        {
+            // if(j==n){
+            retorno = i;
+            // }
+            j++;
+        }
+        i++;
+    }
+    return retorno;
 }
 
 int charToInt(char c)
 {
-    return 0;
+    //'0'=0
+    //'9'=9
+    //'A'=10 = 'a'
+    //'B'=11 = 'b'
+    //'Z'=35 = 'z'
+    int retorno = 0;
+
+    if(c > 47)  // no hace falta, en teoria no es apb
+    {
+        if(c < 58)
+        {
+            retorno = c - 48;
+        }
+        else if(c > 64 && c < 91)
+        {
+            retorno = c - 55;
+        }
+        else if(c > 96 && c < 123)
+        {
+            retorno = c - 87;
+        }
+    }
+
+    return retorno;
 }
 
 char intToChar(int i)
 {
-    return '0';
+    char retorno = '0';
+
+    if(i <= 9)
+    {
+        retorno = i + 48;
+    }
+    else if(i > 9 && i < 36)
+    {
+        retorno = i + 55;
+    }
+    return retorno;
 }
 
 int getDigit(int n, int i)
 {
-    return 0;
+    int retorno;
+    int auxMenos = round(pow(10, i + 1));
+    int aux;
+    aux = n / auxMenos;
+    int parteIzquierda = n - aux * round(pow(10, i + 1));
+    if(i == 0)
+    {
+        retorno = parteIzquierda;  // en este caso no tiene parte derecha
+    }
+    else
+    {
+        retorno = parteIzquierda /
+                  round(pow(10, i));  // en este caso si, y al dividirlo se
+                                      // pierde en la precision
+    }
+
+    return retorno;
 }
 
 int digitCount(int n)
 {
-    return 0;
+    int i = 1;
+    int aux = 1;
+    while(n / aux > 0)
+    {
+        aux = round(pow(10, i));
+        i++;
+    }
+
+    return i - 1;
 }
 
 string intToString(int i)
 {
-    return "";
+    int j = digitCount(i) - 1;
+    string retorno;
+    while(j >= 0)
+    {
+        retorno += intToChar(getDigit(i, j));
+        j--;
+    }
+
+    return retorno;
 }
 
-int stringToInt(string s, int b)  // ok
+int stringToInt(string s, int b)  // ok //revisar si esta muy enredado
 {
-    return 0;
+    // charToInt
+    int retorno = 0;
+    int i = length(s) - 1;
+    int j = 0;
+    while(i >= 0)
+    {
+        int expo = round(pow(b, j));
+        int charEnInt = charToInt(s[i]);  // los puse en variables para debugear
+        retorno += expo * charEnInt;
+        j++;
+        i--;
+    }
+
+    return retorno;
 }
 
 int stringToInt(string s)  // ok
 {
-    return 0;
+    return stringToInt(s, 10);
 }
 
 string charToString(char c)
 {
-    return "";
+    string retorno;
+    retorno += c;
+    return retorno;
 }
 
 char stringToChar(string s)
 {
-    return '0';
+    char retorno = s[0];
+    return retorno;
 }
 
 string stringToString(string s)
 {
-    return "";
+    return s;
 }
 
-string doubleToString(double d)
+string doubleToString(double d)  // Revisar precision
 {
-    return "";
+    int auxParteEntera = int(d);
+    int auxMantisa = (d - auxParteEntera) * round(pow(10, 5));  // harcodeado
+    string retorno;
+
+    retorno += intToString(auxParteEntera);
+    retorno += ".";
+    retorno += intToString(auxMantisa);
+
+    return retorno;
 }
 
 double stringToDouble(string s)
 {
-    return 1.0;
+    double retorno = 0;
+    // substring
+    // indexOf
+
+    int iPunto = indexOf(s, '.');
+    string auxEntero = substring(s, 0, iPunto);
+    string auxMantisa;
+
+    auxMantisa = substring(s, iPunto + 1);
+
+    retorno += stringToInt(auxEntero);
+    retorno +=
+        double(stringToInt(auxMantisa)) / round(pow(10, length(auxMantisa)));
+
+    return retorno;
 }
 
 bool isEmpty(string s)
 {
-    return true;
+    return length(s) > 0 ? false : true;
 }
 
 bool startsWith(string s, string x)
 {
-    return true;
+    bool retorno = true;
+    string inicio = substring(s, 0, length(x) - 1);
+
+    int i = 0;
+    while(i < length(inicio))
+    {
+        if(s[i] != x[i])
+        {
+            retorno = false;
+        }
+        i++;
+    }
+
+    return retorno;
 }
 
 bool endsWith(string s, string x)
 {
-    return true;
+    bool retorno = startsWith(substring(s, length(s) - length(x)), x);
+    return retorno;
 }
 
 bool contains(string s, char c)
 {
-    return true;
+    bool retorno = false;
+
+    if(indexOf(s, c) != -1)
+    {
+        retorno = true;
+    }
+
+    return retorno;
 }
 
 string replace(string s, char oldChar, char newChar)
 {
-    return "";
+    string retorno = s;
+    int i = 0;
+    while(i < length(retorno))
+    {
+        if(retorno[i] == oldChar)
+        {
+            retorno[i] = newChar;
+        }
+        i++;
+    }
+
+    return retorno;
 }
 
 string insertAt(string s, int pos, char c)
 {
-    return "";
+    string retorno;
+
+    retorno += substring(s, 0, pos);
+    retorno += c;
+    retorno += substring(s, pos);
+
+    return retorno;
 }
 
 string removeAt(string s, int pos)
 {
-    return "";
+    string retorno;
+    retorno += substring(s, 0, pos);
+    retorno += substring(s, pos + 1);
+
+    return retorno;
 }
 
 string ltrim(string s)
 {
-    return "";
+    string retorno;
+
+    int i = 0;
+    while(i < length(s))
+    {
+        if(s[i] != ' ')
+        {
+            retorno = substring(s, i);
+            i = length(s);
+        }
+        i++;
+    }
+
+    return retorno;
 }
 
-string rtrim(string s)
+string rtrim(string s)//revisar
 {
-    return "";
+       string retorno;
+
+    int i = length(s);
+    while(i > length(s))
+    {
+        if(s[i] != ' ')
+        {
+            retorno = substring(s, i);
+            i = length(s);
+        }
+        i--;
+    }
+
+    return retorno;
 }
 
 string trim(string s)
@@ -243,7 +569,7 @@ int cmpDouble(double a, double b)
     return 0;
 }
 
-char* stringToCString(string s)
+char* stringToCString(string s)  // Prehecho?
 {
     char* ret = new char[length(s) + 1];
 
--- AlgoritmosAFondo_EMPTY_v2.7.36/principal.cpp
+++ AlgoritmosAFondo_EMPTY_v2.7.36/principal.cpp
@@ -126,9 +126,302 @@ int main()
 // bool consistente = fechaEsValida(f);
 // cout<<consistente<<endl;
 // cout<<fechaDiasEnMes(f)<<endl;
+// int i;
+// cout<< i;
 
-Fecha f = fecha(1900,2,2);
-fechaSetAnio(f,1940);
-cout<<fechaToString(f)<<endl;
+// Fecha f = fecha(1900,2,2);
+// fechaSetAnio(f,1940);
+// cout<<fechaToString(f)<<endl;
+
+
+
+// string s = "Hola";
+// int n = length(s);
+// cout << n << endl; // muestra: 4
+// s = "";
+// n = length(s);
+// cout << n << endl; // muestra: 0
+
+
+
+// string s = "Esto es una prueba";
+
+// int n = charCount(s,'e');
+// cout << n << endl; // muestra: 2
+// n = charCount(s,' ');
+// cout << n << endl; // muestra: 3
+
+//  string s = "Esto es una prueba";
+//  string x = substring(s,2,10);
+//  cout << x << endl; // muestra: to es un
+//  x = substring(s,2,length(s));
+//  cout << x << endl; // muestra: to es una prueba
+
+//   s = "Esto es una prueba";
+//   x = substring(s,2);
+//  cout << x << endl; // muestra: to es una prueba
+
+
+// string s = "Esto es una prueba";
+// int p = indexOf(s,'e');
+// cout << p << endl; // muestra: 5
+// p = indexOf(s,'X');
+// cout << p << endl; // muestra: -1
+
+
+// string s = "Esto es una prueba";
+// int p = indexOf(s,'e',0);
+// cout << p << endl; // muestra: 5
+// p = indexOf(s,'e',12);
+// cout << p << endl; // muestra: 15
+
+// string s = "Esto es una prueba";
+// int p = indexOf(s,"una");
+// cout << p << endl; // muestra: 8
+// p = indexOf(s,"jamon");
+// cout << p << endl; // muestra: algun valor negativo
+
+// string s = "Esta funcion es la funcion mas dificil";
+// int p = indexOf(s,"funcion",0);
+// cout << p << endl; // muestra: 5
+// p = indexOf(s,"funcion",13);
+// cout << p << endl; // muestra: 19
+
+// string s = "John|Paul|George|Ringo";
+
+
+// cout << lastIndexOf(s,'J') << endl; // muestra: 0
+// cout << lastIndexOf(s,'o') << endl; // muestra: 21
+// cout << lastIndexOf(s,'h') << endl; // muestra: 2
+// cout<<"ASDASD"<<endl;
+
+
+//  int p = indexOfN(s,'|',1);
+//  cout << p << endl; // muestra: 4
+//  p = indexOfN(s,'|',2);
+//  cout << p << endl; // muestra: 9
+//  p = indexOfN(s,'|',3);
+//  cout << p << endl; // muestra: 16
+
+
+// char c = '2';
+// int n = charToInt(c); // retorna: 2
+// cout << n << endl;
+// c = 'D';
+// n = charToInt(c); // retorna: 13
+// cout << n << endl; 
+// c = 'd';
+// n = charToInt(c); // retorna: 13
+// cout << n << endl; 
+// c = 'z';
+// n = charToInt(c); // retorna: 13
+// cout << n << endl; 
+
+
+// int i = 2;
+// char c = intToChar(i);
+// cout << c << endl; // muestra: 2
+// i = 13;
+// c = intToChar(i);
+// cout << c << endl; // muestra: D
+
+
+// int n = 12345;
+// int i = 0;
+// int r = getDigit(n,i);
+// cout << r << endl; // muestra: 5
+// i = 1;
+// r = getDigit(n,i);
+// cout << r << endl; // muestra: 4
+// i = 2;
+// r = getDigit(n,i);
+// cout << r << endl; // muestra: 3
+// i = 3;
+// r = getDigit(n,i);
+// cout << r << endl; // muestra: 2
+
+
+
+// int n = 12345;                                                                                                                                                                                  
+// int i = digitCount(n);
+// cout << i << endl; // muestra: 5
+
+// n = 1;                                                                                                                                                                                  
+// i = digitCount(n);
+// cout << i << endl; // muestra: 1
+// n = 11;                                                                                                                                                                                  
+// i = digitCount(n);
+// cout << i << endl; // muestra: 2
+
+
+// int i = 12345;
+// string s = intToString(i);
+// cout << s << endl; // muestra: 12345
+//  i = 1;
+//  s = intToString(i);
+// cout << s << endl; // muestra: 1
+//  i = 125;
+//  s = intToString(i);
+// cout << s << endl; // muestra: 125
+
+
+// string s = "10";
+// int i = stringToInt(s,10);
+// cout << i << endl; // muestra: 10
+// i = stringToInt(s,2);
+// cout << i << endl; // muestra: 2
+// i = stringToInt(s,16);
+// cout << i << endl; // muestra: 16
+// s = "12AB";
+// i = stringToInt(s,16);
+// cout << i << endl; // muestra: 4779
+
+
+// string s = "12345";
+// int i = stringToInt(s);
+// cout << i << endl; // muestra: 12345
+
+
+
+// char c = 'A';
+// string s = charToString(c);
+// cout << s << endl; // muestra: A
+// cout << length(s) << endl; // muestra: 1
+// c = ' ';
+// s = charToString(c);
+// cout << s << endl; // muestra: [VACIO]
+// cout << length(s) << endl; // muestra: 1
+
+
+// string s = "A";
+
+// char c = stringToChar(s);
+// cout << c << endl; // muestra: A
+// cout << (int)c << endl; // muestra: 65
+// s = " ";
+// c = stringToChar(s);
+// cout << c << endl; // muestra: [VACIO]
+// cout << (int)c << endl; // muestra: 32, ASCII de ' '
+
+
+
+
+// string s = stringToString("Hola");
+// cout << s << endl; // muestra: Hola
+
+
+// double d = 123.4;
+// string s = doubleToString(d);
+
+// cout << to_string(d) << endl; // muestra: 123.4
+//  d = 123.4234;
+//  s = doubleToString(d);
+// cout << s << endl; // muestra: 123.4
+//  d = 123.700023;
+//  s = doubleToString(d);
+// cout << s << endl; // muestra: 123.4
+//  d = 123.50234;
+//  s = doubleToString(d);
+// cout << s << endl; // muestra: 123.4
+
+
+//  string s = "123.4";
+// double d = stringToDouble(s);
+// cout << d << endl; // muestra: 123.4
+
+//  s = "123.42";
+//  d = stringToDouble(s);
+// cout << d << endl; // muestra: 123.4
+
+
+//  s = "123.423";
+//  double e = stringToDouble(s);
+// cout << e << endl; // muestra: 123.4
+
+
+// string s = "";
+// cout << isEmpty(s) << endl; // true
+// s = "Hola";
+// cout << isEmpty(s) << endl; // false
+// s = " ";
+// cout << isEmpty(s) << endl; // false
+
+
+// string s1 = "cursoDeAlgoritmos";
+// string s2 = "curso";
+// if( startsWith(s1,s2) )
+// {
+// cout << s2 << " es prefijo de: " << s1 << endl;
+// }
+
+
+
+// string s1 = "cursoDeAlgoritmos";
+// string s2 = "Algoritmos";
+// if( endsWith(s1,s2) )
+// {
+// cout << s2 << " es sufijo de: " << s1 << endl;
+// }
+
+
+// string s = "abcd";
+// char c = 'b';
+// if( contains(s,c) )
+// {
+// cout << s << " contiene a: " << c << endl;
+// }
+// c = 'X';
+// if( !contains(s,c) )
+// {
+// cout << s << " NO contiene a: " << c << endl;
+// }
+
+
+
+// string s = "Esto es una prueba";
+// string r = replace(s,'e','X');
+// cout << r << endl; // SALIDA: Esto Xs una pruXba
+
+
+// string s = "Esto es una prueba";
+// int pos = 6;
+// char c = 'X';
+// string r = insertAt(s,pos,c);
+// cout << r << endl; // SALIDA: Esto eXs una prueba
+
+
+// string s = "Esto es una prueba";
+// int pos = 7;
+// string r = removeAt(s,pos);
+// cout << r << endl; // SALIDA: Esto esuna prueba
+
+
+// // con espacios a izquierda
+// string s = " Esto es una prueba";
+// string r = ltrim(s);
+// cout << "[" << r << "]" << endl; // [Esto es una prueba]
+// // sin espacios
+// s = "Esto es una prueba";
+// r = ltrim(s);
+// cout << "[" << r << "]" << endl; // [Esto es una prueba]
+// // con espacios a izquierda y derecha
+// s = " Esto es una prueba ";
+// r = ltrim(s);
+// cout << "[" <<length(s) r << "]" << endl; // [Esto es una prueba ]
+
+
+// con espacios a derecha
+string s = "Esto es una prueba ";
+string r = rtrim(s);
+cout << "[" << r << "]" << endl; // [Esto es una prueba]
+// sin espacios
+s = "Esto es una prueba";
+r = rtrim(s);
+cout << "[" << r << "]" << endl; // [Esto es una prueba]
+// con espacios a izquierda y derecha
+s = " Esto es una prueba ";
+r = rtrim(s);
+cout << "[" << r << "]" << endl; // [ Esto es una prueba]
+
     return 0;
 }
